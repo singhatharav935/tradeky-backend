e60084582919d5eaf2115934d6c36125708f56d6
singhatharav935 <singhatharav935@gmail.com>
2025-12-29 00:59:54 +0530

    Fix authMiddleware path in protectedRoutes

--- src/routes/protectedRoutes.js
+++ src/routes/protectedRoutes.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const protect = require('../middleware/authMiddleware');
+const protect = require('../middlewares/authMiddleware');
+
 
 const router = express.Router();
 
